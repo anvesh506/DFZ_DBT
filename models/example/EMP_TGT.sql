d06753a0e4dfac8c91e2a3ea5b2e5bcd27699b48
anvesh.chintakunta@datafactz.com <anvesh.chintakunta@datafactz.com>
2022-05-16 04:53:20 +0000

    emp added to code

--- models/example/EMP_TGT.sql
+++ models/example/EMP_TGT.sql
@@ -5,7 +5,7 @@
     )
 }}
 
-SELECT EMP_ID, EMP_NAME,INSERT_DT FROM EMP
+SELECT EMP_ID, EMP_NAME,INSERT_DT FROM PC_DBT_DB.PUBLIC.EMP
 {% if is_incremental() %}
   -- this filter will only be applied on an incremental run
   where INSERT_DT > (SELECT MAX(INSERT_DT) from PC_DBT_DB.PUBLIC.EMP_tgt )
