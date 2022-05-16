dddcde4e28f3dce3eb4ecf87b7a2c4ee8034825a
anvesh.chintakunta@datafactz.com <anvesh.chintakunta@datafactz.com>
2022-05-16 04:31:35 +0000

    dd

---
+++ models/example/EMP_TGT.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='INSERT_DT'
+    )
+}}
+
+SELECT EMP_ID, EMP_NAME,INSERT_DT FROM EMP
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where INSERT_DT > (SELECT MAX(INSERT_DT) from PC_DBT_DB.PUBLIC.EMP_tgt )
+
+{% endif %}
+
